db647d066dd0d3530ecc0ce127ea8a31b6f67075
Shreyansh Pandey <s@isomr.co>
2018-10-13 02:03:15 +0530

    DO NOT DO THIS. PLEASE CHANGE YOUR PASSWORD AND MAKE THIS RUNTIME ENVIRONMENT VARIABLES.

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,6 @@
+// THIS IS VERY INSECURE.
+// USE RUNTIME VARIABLES.
+
 module.exports = {
   mongoURI: "mongodb://mohit:mohit12345@ds123852.mlab.com:23852/alpha",
   secretOrKey: "dafuuuuuuuuuuuuuuuuuuuuqqqqqqqqqqq"
